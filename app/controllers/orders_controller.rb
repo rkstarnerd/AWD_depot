1658909303079e4a1809854609325ca503e2af63
Gemille Ford <gemille.ford@gmail.com>
2015-01-30 15:52:40 -0500

    edit new action for order and correct form field type

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -25,6 +25,7 @@ class OrdersController < ApplicationController
     respond_to do |format|
       format.html # new.html.erb
       format.xml { render :xml => @order }
+    end
   end
 
   # GET /orders/1/edit
