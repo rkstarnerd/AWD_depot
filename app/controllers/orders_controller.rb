77bb84eed719fcaad51a9e20ea995a77412ba208
Gemille Ford <gemille.ford@gmail.com>
2015-01-30 12:09:36 -0500

    add order button and form

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -14,7 +14,17 @@ class OrdersController < ApplicationController
 
   # GET /orders/new
   def new
+    @cart = current_cart
+    if @cart.line_items.empty?
+      redirect_to store_url, :notice => "Your cart is empty"
+      return
+    end
+
     @order = Order.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml { render :xml => @order }
   end
 
   # GET /orders/1/edit
