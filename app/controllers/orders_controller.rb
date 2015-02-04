a39177662d941c8771768955cd0798d2a6f0441a
Gemille Ford <gemille.ford@gmail.com>
2015-02-04 12:36:41 -0500

    corrected orders pagination in index action for orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,7 +4,7 @@ class OrdersController < ApplicationController
   # GET /orders
   # GET /orders.json
   def index
-    @orders = Order.paginate :page=>params[:page], :order=>'created_at desc', :per_page => 10
+    @orders = Order.paginate :page=>params[:page], :per_page => 10
 
     respond_to do |format|
       format.html # index.html.erb
