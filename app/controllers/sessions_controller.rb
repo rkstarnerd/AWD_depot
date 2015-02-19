a799fa3d314405192f82b5db9f917d1b13e58ddb
Gemille Ford <gemille.ford@gmail.com>
2015-02-19 13:09:00 -0500

    modify session create action to authenticate admin users and log in view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,12 @@ class SessionsController < ApplicationController
   end
 
   def create
+    if user = User.authenticate(params[:name], params[:password])
+      session[:user_id] = user.id
+      redirect_to admin_url
+    else
+      redirect_to login_url, :alert => "Invalid user/password combination"
+    end
   end
 
   def destroy
